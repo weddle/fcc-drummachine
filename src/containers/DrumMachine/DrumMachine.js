2d2734650407604d774fef5add426b7476d0c662
Ryan Weddle <weddle@gmail.com>
2020-01-06 10:01:44 -0500

    Added descriptions for sounds in keyConfig in DrumMachine.js

--- src/containers/DrumMachine/DrumMachine.js
+++ src/containers/DrumMachine/DrumMachine.js
@@ -5,56 +5,65 @@ import Display from "../../components/Display/Display";
 
 import classes from "./DrumMachine.module.css";
 
-const keys = [
+const keyConfig = [
   {
     id: "qsound",
     letter: "Q",
-    audioFile: "/assets/sounds/Bass-Drum-1.wav"
+    audioFile: "/assets/sounds/Bass-Drum-1.wav",
+    description: "Bass Drum 1"
   },
   {
     id: "wsound",
     letter: "W",
-    audioFile: "/assets/sounds/Bass-Drum-2.wav"
+    audioFile: "/assets/sounds/Bass-Drum-2.wav",
+    description: "Bass Drum 2"
   },
   {
     id: "esound",
     letter: "E",
-    audioFile: "/assets/sounds/Bass-Drum-3.wav"
+    audioFile: "/assets/sounds/Bass-Drum-3.wav",
+    description: "Bass Drum 3"
   },
   {
     id: "asound",
     letter: "A",
-    audioFile: "/assets/sounds/E-Mu-Proteus-FX-909-Kick.wav"
+    audioFile: "/assets/sounds/E-Mu-Proteus-FX-909-Kick.wav",
+    description: "E-Mu-Proteus-FX 909 Kick"
   },
   {
     id: "ssound",
     letter: "S",
-    audioFile: "/assets/sounds/E-Mu-Proteus-FX-Wacky-Kick.wav"
+    audioFile: "/assets/sounds/E-Mu-Proteus-FX-Wacky-Kick.wav",
+    description: "E-Mu-Proteus-FX Wacky Kick"
   },
   {
     id: "dsound",
     letter: "D",
-    audioFile: "/assets/sounds/E-Mu-Proteus-FX-Wacky-Snare.wav"
+    audioFile: "/assets/sounds/E-Mu-Proteus-FX-Wacky-Snare.wav",
+    description: "E-Mu-Proteus-FX Wacky Snare"
   },
   {
     id: "zsound",
     letter: "Z",
-    audioFile: "/assets/sounds/Ensoniq-SQ-1-Closed-Hi-Hat.wav"
+    audioFile: "/assets/sounds/Ensoniq-SQ-1-Closed-Hi-Hat.wav",
+    description: "Ensoniq SQ-1 Closed Hi-Hat"
   },
   {
     id: "xsound",
     letter: "X",
-    audioFile: "/assets/sounds/Ensoniq-SQ-1-Open-Hi-Hat.wav"
+    audioFile: "/assets/sounds/Ensoniq-SQ-1-Open-Hi-Hat.wav",
+    description: "Ensoniq SQ-1 Open Hi-Hat"
   },
   {
     id: "csound",
     letter: "C",
-    audioFile: "/assets/sounds/Ensoniq-SQ-1-Side-Stick.wav"
+    audioFile: "/assets/sounds/Ensoniq-SQ-1-Side-Stick.wav",
+    description: "Ensoniq SQ-1 Side Stick"
   }
 ];
 class DrumMachine extends Component {
   render() {
-    let renderedKeys = keys.map((k) => (
+    let renderedKeys = keyConfig.map((k) => (
       <DrumPad id={k.id} letter={k.letter} audioFile={k.audioFile} />
     ));
     return (
