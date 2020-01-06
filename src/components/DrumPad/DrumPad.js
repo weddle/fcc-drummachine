64eddd7d6130779c14117471db8cab14b62f1773
Ryan Weddle <weddle@gmail.com>
2020-01-06 11:17:46 -0500

    Updated manifest, removed unneeded logging

--- src/components/DrumPad/DrumPad.js
+++ src/components/DrumPad/DrumPad.js
@@ -4,14 +4,11 @@ import "./DrumPad.css";
 
 class DrumPad extends Component {
   clickHandler = (event) => {
-    console.log(event);
-
     // if a setDisplay handler is passed, set the display
     this.props.setDisplay && this.props.setDisplay();
     this.props.mediaRef.current.play();
   };
   render() {
-    console.log(this.props);
     return (
       <div className="drum-pad" id={this.props.id} onClick={this.clickHandler}>
         <p>{this.props.letter}</p>
